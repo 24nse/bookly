6dafd2f2476da40818b8768477c4f4648767a810
nooh saeed <noohsaeed240@gmail.com>
2025-06-22 14:28:02 +0300

    best seller list view item part one

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/featured_list_view.dart';
@@ -18,10 +19,38 @@ class HomeViewBody extends StatelessWidget {
           CustomAppBar(),
           FeatutedBooksListView(),
           SizedBox(height: 50,),
-          Text("Best Seller", style: Styles.titleMedium,)
+          Text("Best Seller", style: Styles.titleMedium,),
+          BestSellerListViewItem(),
           ],
       
       ),
     );
   }
 } 
+
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio( 
+            aspectRatio: 2.5 /4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.red,
+                image:const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.testImage))
+              ),
+              ))
+        ],
+      ),
+    );
+  }
+}
