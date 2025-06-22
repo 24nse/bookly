5cb83f4f924eae4ed9310fe0e9462f698f11db9d
nooh saeed <noohsaeed240@gmail.com>
2025-06-22 15:21:31 +0300

    best seller item done

--- lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -3,6 +3,7 @@
 import 'package:bookly/conastans.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:flutter/material.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
@@ -28,33 +29,37 @@ class BestSellerListViewItem extends StatelessWidget {
             ),
           ),
           SizedBox(width: 30),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .5,
-                child:  Text(
-                  "Harry Potter and the Goblet of Fire",
-                  style: Styles.textStyle20.copyWith(
-                    fontFamily: kGtSectraFine
-                  ),
-                  maxLines: 2,
-                  overflow:TextOverflow.ellipsis ,
-                ),
-              ),
-             const SizedBox(height: 3,),
-             const Text("J.K.Rowling",style: Styles.titleStyle14,),
-                                       const SizedBox(height: 3,),
-
-             Row(
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("19.99\$",style: Styles.textStyle20.copyWith(
-                  fontWeight: FontWeight.bold
-                ),)
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * .5,
+                  child:  Text(
+                    "Harry Potter and the Goblet of Fire",
+                    style: Styles.textStyle20.copyWith(
+                      fontFamily: kGtSectraFine
+                    ),
+                    maxLines: 2,
+                    overflow:TextOverflow.ellipsis ,
+                  ),
+                ),
+               const SizedBox(height: 3,),
+               const Text("J.K.Rowling",style: Styles.titleStyle14,),
+                                         const SizedBox(height: 3,),
+            
+               Row(
+                children: [
+                  Text("19.99\$",style: Styles.textStyle20.copyWith(
+                    fontWeight: FontWeight.bold
+                  ),),
+                 const Spacer(),
+                const  BookRating(),
+                ],
+               ),
+            
               ],
-             ),
-
-            ],
+            ),
           ),
         ],
       ),
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -5,6 +5,8 @@ import 'package:bookly/features/home/presentation/views/widgets/best_seller_list
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -27,3 +29,4 @@ class HomeViewBody extends StatelessWidget {
     );
   }
 }
+
