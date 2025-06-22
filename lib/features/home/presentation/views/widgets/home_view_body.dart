2f5ef7e9b4d2792d1f50daeadbe2db3c139b1f82
nooh saeed <noohsaeed240@gmail.com>
2025-06-22 14:53:20 +0300

    best seller item part 2

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,10 +1,8 @@
-
 import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:flutter/material.dart';
-
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -12,43 +10,58 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+      padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
-        crossAxisAlignment:  CrossAxisAlignment.start,
-          children: const[
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
           CustomAppBar(),
           FeatutedBooksListView(),
-          SizedBox(height: 50,),
-          Text("Best Seller", style: Styles.textStyle18,),
+          SizedBox(height: 50),
+          Text("Best Seller", style: Styles.textStyle18),
+          SizedBox(height: 20),
           BestSellerListViewItem(),
-          ],
-      
+        ],
       ),
     );
   }
-} 
-
+}
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  SizedBox(
+    return SizedBox(
       height: 125,
       child: Row(
         children: [
-          AspectRatio( 
-            aspectRatio: 2.5 /4,
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
             child: Container(
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(8),
                 color: Colors.red,
-                image:const DecorationImage(
+                image: const DecorationImage(
                   fit: BoxFit.fill,
-                  image: AssetImage(AssetsData.testImage))
+                  image: AssetImage(AssetsData.testImage),
+                ),
               ),
-              ))
+            ),
+          ),
+          SizedBox(width: 30),
+          Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                child: const Text(
+                  "Harry Potter and the Goblet of Fire",
+                  style: Styles.textStyle20,
+                  maxLines: 2,
+                  overflow:TextOverflow.ellipsis ,
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
