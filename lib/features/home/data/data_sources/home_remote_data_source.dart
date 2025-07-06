940dbb1363f3d2aae198e5ade9fa246f95e43e3a
nooh saeed <noohsaeed240@gmail.com>
2025-07-06 12:59:30 +0300

    cache featured books

--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,45 +1,49 @@
+import 'package:bookly/conastans.dart';
 import 'package:bookly/core/utils/api_service.dart';
+import 'package:bookly/core/utils/function/save_books.dart';
 import 'package:bookly/features/home/data/models/models/book_model/book_model.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:hive/hive.dart';
 
 abstract class HomeRemoteDataSource {
   Future<List<BookEntity>> fetchFeaturedBooks();
   Future<List<BookEntity>> fetchNewsetBooks();
 }
-class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
 
-final ApiService apiService;
+class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  final ApiService apiService;
 
-  HomeRemoteDataSourceImpl(this.apiService); 
+  HomeRemoteDataSourceImpl(this.apiService);
 
   @override
-  Future<List<BookEntity>> fetchFeaturedBooks()async {
-   
-      var data = await apiService.get(
-        endPoint: "volumes?Filtering=free-ebooks&q=subject:Programming",
-      );
-      List<BookEntity> books = getBooksList(data);
-      return books;
-     
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    var data = await apiService.get(
+      endPoint: "volumes?Filtering=free-ebooks&q=subject:Programming",
+    );
+    List<BookEntity> books = getBooksList(data);
+
+    saveBooksData(books,KFeaturedBox);
+
+    return books;
   }
 
   
 
   @override
-  Future<List<BookEntity>> fetchNewsetBooks()async {
-     var data = await apiService.get(
-        endPoint:  "volumes?Filtering=free-ebooks&Sorting=newest&q=subject:computer science",
-      );
-      List<BookEntity> books = getBooksList(data);
-      return books;
-  }
-
-}
-
-  List<BookEntity> getBooksList(Map<String, dynamic> data) {
-         List<BookEntity> books = [];
-    for (var item in data["items"]) {
-      books.add(BookModel.fromJson(item));
-    }
+  Future<List<BookEntity>> fetchNewsetBooks() async {
+    var data = await apiService.get(
+      endPoint:
+          "volumes?Filtering=free-ebooks&Sorting=newest&q=subject:computer science",
+    );
+    List<BookEntity> books = getBooksList(data);
     return books;
   }
+}
+
+List<BookEntity> getBooksList(Map<String, dynamic> data) {
+  List<BookEntity> books = [];
+  for (var item in data["items"]) {
+    books.add(BookModel.fromJson(item));
+  }
+  return books;
+}
